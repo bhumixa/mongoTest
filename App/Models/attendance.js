b1376f688c1ae93b1bf5855359c31f28ce8cf2d2
bhumixa <bhumi.patel2512@gmail.com>
2016-02-04 22:45:14 +0530

    valid password changes

--- App/Models/attendance.js
+++
@@ -1,7 +0,0 @@
-var userSchema = mongoose.Schema({
-    id: { type: String},  // company user Id (id of tenant)
-    name:{ type: String, default: '' },
-    password:{ type: String}
-});
-
-module.exports = mongoose.model('User', userSchema, 'user');
